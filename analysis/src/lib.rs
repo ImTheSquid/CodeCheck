5b2bb9d5a774bdd4070692207205ae18b1eb928c
Jack Hogan <jackhogan11@gmail.com>
2023-09-08 11:13:03 -0400

    Added basic traits and loader functions

--- analysis/src/lib.rs
+++ analysis/src/lib.rs
@@ -1,16 +1,79 @@
+use std::{path::PathBuf, fs};
+
+use thiserror::Error;
+use anyhow::Result;
+
 mod gen;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/// Big-O runtime complexity
+pub enum RuntimeComplexity {
+    /// O(1)
+    Constant,
+    /// O(log(n))
+    Logarithmic,
+    /// O(n)
+    Linear,
+    /// O(nlog(n))
+    SuperLinear,
+    /// O(n^a)
+    Polynomial(usize),
+    /// O(2^n)
+    Exponential,
+    /// O(n!)
+    Factorial,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Represents any tree for a specific language
+pub trait SyntaxTree {
+    /// Compares a tree with another tree of the specific language
+    fn compare(&self, other: &Self) -> f64;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// Gets the worst runtime complexity within the tree
+    fn worst_runtime_complexity(&self) -> RuntimeComplexity;
+
+    /// Gets the runtime complexity of a single function, returns `Option::None` if not found
+    fn runtime_complexity_of_fn<S: AsRef<str>>(&self, name: S) -> Option<RuntimeComplexity>;
+}
+
+/// Any errors that may occur when generating a parse tree
+#[derive(Debug, Error)]
+pub enum TreeParseError {
+    #[error(transparent)]
+    FileError(std::io::Error),
+    #[error("Unknown language")]
+    UnknownLanguage,
+}
+
+/// The language to be parsed
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Language {
+    Java,
+    C,
+    Cpp,
+    Python,
+}
+
+/// Attempts to parse a language and its tree from a string
+pub fn generate_tree<S: AsRef<str>, T: SyntaxTree>(input: S, language: Language) -> Result<T, TreeParseError> {
+    todo!()
+}
+
+/// Attempts to parse a language and its tree from a file
+pub fn generate_tree_from_file<P: Into<PathBuf>, T: SyntaxTree>(input: P, language: Option<Language>) -> Result<T, TreeParseError> {
+    let buf = input.into();
+    generate_tree(fs::read_to_string(buf.clone()).map_err(TreeParseError::FileError)?, match language {
+        Some(l) => l,
+        None => guess_language_from_path(buf)?,
+    })
+}
+
+/// Attempts to guess the language of the file using a path
+fn guess_language_from_path(path: PathBuf) -> Result<Language, TreeParseError> {
+    match path.extension().ok_or(TreeParseError::UnknownLanguage)?.to_str().ok_or(TreeParseError::UnknownLanguage)? {
+        "java" => Ok(Language::Java),
+        "py" => Ok(Language::Python),
+        "c" | "h" => Ok(Language::C),
+        "cpp" | "cc" | "hh" | "cxx" | "hpp" | "hxx" => Ok(Language::Cpp),
+        _ => Err(TreeParseError::UnknownLanguage),
     }
 }
