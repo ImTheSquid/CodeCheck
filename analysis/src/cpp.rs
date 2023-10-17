6e2bc72059574821a4b1fdb2865db0e0c93ca6c6
Ava Lyall <avalyall@pal-nat186-15-96.itap.purdue.edu>
2023-10-17 16:20:00 -0400

    Initial Implementation (no test cases yet)

--- analysis/src/cpp.rs
+++ analysis/src/cpp.rs
@@ -1,15 +1,33 @@
-use antlr_rust::tree::ParseTreeVisitorCompat;
+use antlr_rust::InputStream;
+use antlr_rust::common_token_stream::CommonTokenStream;
+use antlr_rust::tree::{ErrorNode, ParseTreeVisitorCompat, TerminalNode};
 use syntree::Tree;
+
+use crate::gen::cpp14lexer::CPP14Lexer;
+use crate::gen::cpp14parser::*;
+use crate::gen::cpp14parservisitor::CPP14ParserVisitorCompat;
+use crate::{SyntaxTree, VisitorReturn, TreeParseError, visitor_result};
+
 use macros::auto_visitor;
 
-use crate::gen::cpp14parser::*;
-use crate::visitor_result;
-use crate::gen::cpp14parservisitor::CPP14ParserVisitorCompat;
-use crate::{VisitorReturn, SyntaxTree, TreeParseError};
-
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct CppTree {
-    symbol_tree: syntree::Builder<CppTreeItem, usize, usize>,
+    /// Contains all necessary indices to reconstruct the source code from the original with
+    /// symbols. This tree also contains whitespace and variable names, so it may not work as
+    /// well for comparisons.
+    /// TODO: Figure if non-token structure tree is needed
+    pub symbol_tree: syntree::Builder<CppTreeItem, usize, usize>,
+    /// Temporary variable for visitor
+    tmp: VisitorReturn<()>,
+}
+
+impl Clone for CppTree{
+    fn clone(&self) -> Self{
+        Self{
+            symbol_tree: self.symbol_tree.clone(),
+            tmp: Default::default(),
+        }
+    }
 }
 
 impl ParseTreeVisitorCompat<'_> for CppTree {
@@ -18,6 +36,20 @@ impl ParseTreeVisitorCompat<'_> for CppTree {
 
     fn temp_result(&mut self) -> &mut Self::Return {
         Box::leak(Box::default())
+    }
+
+    fn visit_terminal(&mut self, node: &TerminalNode<'_, Self::Node>) -> Self::Return {
+        if node.symbol.start - *self.symbol_tree.cursor() as isize > 0 {
+            visitor_result!(self.symbol_tree.token(CppTreeItem::Whitespace, node.symbol.start as usize - self.symbol_tree.cursor()));
+        }
+
+        visitor_result!(self.symbol_tree.token(CppTreeItem::Terminal, node.symbol.text.len()));
+
+        VisitorReturn(Ok(()))
+    }
+
+    fn visit_error_node(&mut self, _node: &ErrorNode<'_, Self::Node>) -> Self::Return {
+        VisitorReturn(Err(TreeParseError::InvalidNode))
     }
 }
 
@@ -34,6 +66,18 @@ impl TryFrom<String> for CppTree {
     type Error = TreeParseError;
 
     fn try_from(value: String) -> Result<Self, Self::Error> {
-        todo!()
+        let lexer = CPP14Lexer::new(InputStream::new(value.as_str()));
+        let mut parser = CPP14Parser::new(CommonTokenStream::new(lexer));
+
+        let root = parser.translationUnit()?;
+
+        let mut tree = CppTree {
+            symbol_tree: Default::default(),
+            tmp: Default::default(),
+        };
+
+        tree.visit(&*root).0?;
+
+        Ok(tree)
     }
 }
