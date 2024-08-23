465d9da1cab426e1d42f85ed3366142c554268db
Jack Hogan <jackhogan11@gmail.com>
2024-08-23 15:59:19 -0400

    Added LeakyReLU

--- neural/src/node_process.rs
+++ neural/src/node_process.rs
@@ -1,7 +1,7 @@
 use burn::{
     config::Config,
     module::Module,
-    nn::{Linear, LinearConfig, Relu},
+    nn::{LeakyRelu, LeakyReluConfig, Linear, LinearConfig, Relu},
     prelude::Backend,
     tensor::Tensor,
 };
@@ -10,7 +10,10 @@ use burn::{
 #[derive(Debug, Module)]
 struct NodeProcessor<B: Backend> {
     linear1: Linear<B>,
+    // Leaky to allow for negatives but to an extent
+    activation1: LeakyRelu<B>,
     linear2: Linear<B>,
+    activation2: LeakyRelu<B>,
     linear3: Linear<B>,
     activation: Relu,
 }
@@ -18,8 +21,8 @@ struct NodeProcessor<B: Backend> {
 impl<B: Backend> NodeProcessor<B> {
     fn forward(&self, x: Tensor<B, 1>) -> Tensor<B, 1> {
         let x = self.linear1.forward(x);
-        let x = self.linear2.forward(x);
-        let x = self.linear3.forward(x);
+        let x = self.linear2.forward(self.activation1.forward(x));
+        let x = self.linear3.forward(self.activation2.forward(x));
         self.activation.forward(x)
     }
 }
@@ -27,7 +30,9 @@ impl<B: Backend> NodeProcessor<B> {
 #[derive(Debug, Config)]
 struct NodeProcessorConfig {
     hidden_1_size: usize,
+    leaky_1_slope: f64,
     hidden_2_size: usize,
+    leaky_2_slope: f64,
     output_size: usize,
 }
 
@@ -39,7 +44,13 @@ impl NodeProcessorConfig {
             // Add one for language identifier
             linear1: LinearConfig::new(MAX_ONE_HOT_TREE_ENCODING_LEN + 1, self.hidden_1_size)
                 .init(device),
+            activation1: LeakyReluConfig::new()
+                .with_negative_slope(self.leaky_1_slope)
+                .init(),
             linear2: LinearConfig::new(self.hidden_1_size, self.hidden_2_size).init(device),
+            activation2: LeakyReluConfig::new()
+                .with_negative_slope(self.leaky_2_slope)
+                .init(),
             linear3: LinearConfig::new(self.hidden_2_size, self.output_size).init(device),
             activation: Relu::new(),
         }
