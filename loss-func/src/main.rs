7d112e721601edff0a8bfdaabe6654c0b0778de6
Ban-Ironic-Ohms <micahrobinson93@gmail.com>
2024-09-27 12:51:33 -0400

    before major change

--- loss-func/src/main.rs
+++ loss-func/src/main.rs
@@ -1,7 +1,6 @@
 use burn::tensor::Tensor;
 use burn::backend::Wgpu;
 use burn::prelude::Float;
-use std::ops::Rem;
 use burn::prelude::Int;
 
 
@@ -17,25 +16,38 @@ fn loss_sum(truths: &Tensor<Backend, 2>, predicts: &Tensor<Backend, 2>) -> f64 {
     let len = truths.dims()[0];
     println!("Length of truths matrix (num of truths): {}", len);
 
+    let mut used = <Vec<usize>>::new();
+    // let mut used = <Vec<i32>>::new();
+
     for i in 0..len {
         // let mut max: f64 = loss(&truths.select(0, Tensor::<Backend, 1>::from_floats()), &predicts.select(0, i));
-        let idx_0 = Tensor::<Backend, 1, Int>::from_ints([0, 1], &truths.device());
-        let idx_i = Tensor::<Backend, 1, Int>::from_ints([i, i+1], &truths.device());
-        let mut max: f64 = loss(&truths.select(1, idx_0), &predicts.select(0, idx_i));
+        let idx_0 = Tensor::<Backend, 1, Int>::from_ints([0], &truths.device());
+        let idx_i = Tensor::<Backend, 1, Int>::from_ints([i], &truths.device());
+        let mut max: f64 = loss(&truths.select(1, idx_0).flatten::<1>(0, 1), &predicts.select(0, idx_i).flatten::<1>(0, 1));
 
         let mut max_truth_idx = 0;
         println!("Searching through truths: {:?} for better loss than loss: {}", truths, max);
         for j in 0..truths.dims()[0] {
-            let idx_j = Tensor::<Backend, 1, Int>::from_ints([j, j+1], &truths.device());
-            let loss = loss(&truths.select(0, idx_j), &predicts.select(0, idx_i));
-            println!("Truth: {:?}, Predict: {:?}, Loss: {}", truths.select(0, idx_j), predicts.select(0, idx_i), loss);
-            if loss > max {
-                println!("Found best match with loss: {}", loss);
-                max = loss;
-                max_truth_idx = j;
+            if !used.contains(&j) {
+                let idx_j = Tensor::<Backend, 1, Int>::from_ints([j], &truths.device());
+                let loss = loss(&truths.select(0, idx_j).flatten::<1>(0, 1), &predicts.select(0, idx_i).flatten::<1>(0, 1));
+                println!("Truth: {:?}, Predict: {:?}, Loss: {}", truths.select(0, idx_j), predicts.select(0, idx_i), loss);
+                if loss > max {
+                    println!("Found best match with loss: {}", loss);
+                    max = loss;
+                    max_truth_idx = j;
+                }
             }
+
         }
-        truths.rem(max_truth_idx);
+        
+        used.push(max_truth_idx);
+
+        // truths = truths.dims().iter()
+        //     .enumerate()
+        //     .filter(|&(i, _)| i != max_truth_idx)
+        //     .map(|(_, row)| row.clone())
+        //     .collect();
 
         sum += max;
     }
@@ -44,19 +56,19 @@ fn loss_sum(truths: &Tensor<Backend, 2>, predicts: &Tensor<Backend, 2>) -> f64 {
 
 }
 
-fn loss(truth: &Tensor<Backend, 2>, predict: &Tensor<Backend, 2>) -> f64 {
+fn loss(truth: &Tensor<Backend, 1>, predict: &Tensor<Backend, 1>) -> f64 {
     // Assign values
-    let t_l1 = truth.select(0, Tensor::<Backend, 1, Int>::from_ints([0 ,0+1], &truth.device()));
-    let t_l2 = truth.select(0, Tensor::<Backend, 1, Int>::from_ints([1 ,1+1], &truth.device()));
-    let t_r1= truth.select(0, Tensor::<Backend, 1, Int>::from_ints([ 2, 21], &truth.device()));
-    let t_r2= truth.select(0, Tensor::<Backend, 1, Int>::from_ints([ 3, 31], &truth.device()));
+    let t_l1 = truth.select(0, Tensor::<Backend, 1, Int>::from_ints([0], &truth.device())).into_scalar();
+    let t_l2 = truth.select(0, Tensor::<Backend, 1, Int>::from_ints([1], &truth.device())).into_scalar();
+    let t_r1= truth.select(0, Tensor::<Backend, 1, Int>::from_ints([2], &truth.device())).into_scalar();
+    let t_r2= truth.select(0, Tensor::<Backend, 1, Int>::from_ints([3], &truth.device())).into_scalar();
 
-    let p_l1 = predict.select(0, Tensor::<Backend, 1, Int>::from_ints([0 ,0+1], &truth.device()));
-    let p_l2 = predict.select(0, Tensor::<Backend, 1, Int>::from_ints([1 ,1+1], &truth.device()));
-    let p_r1 = predict.select(0, Tensor::<Backend, 1, Int>::from_ints([2 ,2+1], &truth.device()));
-    let p_r2 = predict.select(0, Tensor::<Backend, 1, Int>::from_ints([3 ,3+1], &truth.device()));
+    let p_l1 = predict.select(0, Tensor::<Backend, 1, Int>::from_ints([0], &truth.device())).into_scalar();
+    let p_l2 = predict.select(0, Tensor::<Backend, 1, Int>::from_ints([1], &truth.device())).into_scalar();
+    let p_r1 = predict.select(0, Tensor::<Backend, 1, Int>::from_ints([2], &truth.device())).into_scalar();
+    let p_r2 = predict.select(0, Tensor::<Backend, 1, Int>::from_ints([3], &truth.device())).into_scalar();
 
-    
+
 
     // let t_l1 = truth[0];
     // let t_l2 = truth[1];
@@ -74,11 +86,11 @@ fn loss(truth: &Tensor<Backend, 2>, predict: &Tensor<Backend, 2>) -> f64 {
 
     let loss = l_giou + r_giou; // this is where we would apply function g
 
-    return loss;
+    return loss.into();
 }
 
 #[allow(dead_code)]
-fn giou(t1: f64, t2: f64, p1: f64, p2: f64) -> f64 {
+fn giou(t1: f32, t2: f32, p1: f32, p2: f32) -> f32 {
     // println!("t1: {}, t2: {}, p1: {}, p2: {}", t1, t2, p1, p2);
     let intersection = (t2.min(p2) - t1.max(p1)).max(0.0);
     // println!("Intersection: {}", intersection);
@@ -108,34 +120,22 @@ fn main() {
     let truth = &mut Tensor::<Backend, 2, Float>::from_floats(truth_floats, &device);
     let predict = Tensor::<Backend, 2, Float>::from_floats(predict_floats, &device);
 
-    // // let truth: &mut Vec<Vec<f64>> = &mut vec![vec![27.0, 33.0, 30.0, 35.0], vec![3.0, 9.0, 4.0, 10.0]];
-    // // let predict: [[f64; 4]; 3] = [[3.0, 10.5, 4.0, 10.0], [27.5, 33.01, 29.99, 34.45], [12.3, 14.2, 33.5, 33.7]];
-
-    // let loss = loss_sum(truth, &predict);
-    // println!("Loss: {}", loss);
-
-
 
     // let floats = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
-
-
-
-    // correct: Tensor is 1-Dimensional with 5 elements
-    // let tensor_1 = Tensor::<Backend, 1, Float>::from_floats(floats, &device);
-
-    let num_predictions: usize = predict.dims()[0];
-
-    // let first_row = predict.slice(0);
-    // println!("{:?}", first_row);
-    // println!("length of preds (should be 3): {}", num_predictions);
-
-    // let loss = loss_sum(truth, &predict);
     
+    // print the predict tensor
+    let mut used: Vec<i32> = Vec::new();
+    used.push(1);
+    println!("{:?}", used);
+    if used.contains(&1) {
+        println!("Found 1 in used");
+    }
+    let tensor_str = predict.flatten::<1>(0, 1).select(0, Tensor::<Backend, 1, Int>::from_ints([2], &device)).into_scalar();
 
-    // println!("{:?}", tensor_1.dims().iter().product());
-    // print tensor_1 values
-    // println!("{:?}", tensor_1.slice([1]));
-    // println!("{:?}", truth.clone().slice(1));
-    // println!("{:?}", predict.clone().all());
+    println!("{}", tensor_str);
+    // println!("{:?}", predict);
+    
+    // print the predict tensor flattened to 1d
+    // println!("{:?}", predict.flatten::<2>(0, 1));
 
 }
